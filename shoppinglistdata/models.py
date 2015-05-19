a58a960ef875970a5b4a84492b168a601f9e0bd0
Jonathan Raymond <jonr@new-host-2.home>
2015-05-18 21:31:01 -0400

    save user

--- shoppinglistdata/models.py
+++ shoppinglistdata/models.py
@@ -20,7 +20,7 @@ class User(models.Model):
     password = models.CharField(max_length=100)
 
     def __str__(self):
-        return str(username)
+        return self.username
 
 class ItemList(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)

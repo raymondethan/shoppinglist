d422ba17bcec079c6bc3d1dbe6fdfc6767c503b1
Jonathan Raymond <jonr@new-host-2.home>
2015-05-19 17:20:17 -0400

    username

--- shoppinglist/urls.py
+++ shoppinglist/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     url(r'^$', shoppinglistdata.views.index, name='index'),
     url(r'^newitem/', shoppinglistdata.views.newitem, name='newitem'),
     url(r'^newuser/', shoppinglistdata.views.newuser, name='newuser'),
-    url(r'^getlist/(?P<username>.*)/$', shoppinglistdata.views.getlist, name='getlist')
+    url(r'^getlist/', shoppinglistdata.views.getlist, name='getlist')
 ]
 #(?P<shoppingitem_id>[0-9]+)/

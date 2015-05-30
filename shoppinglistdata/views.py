77b984da152419c99faecaba5853d5d9c1a4b7ad
Ethan Raymond <ethan.raymond14@gmail.com>
2015-05-30 17:41:00 -0500

    login

--- shoppinglistdata/views.py
+++ shoppinglistdata/views.py
@@ -25,13 +25,9 @@ def newitem(request):
         item.item_name = json_data['item']
         item.save()
         itemlist = ItemList()
-        print("hi")
         itemlist.item_key = item
         itemlist.user_key = User.objects.filter(username=json_data['username'])[0]
         itemlist.list_key = ShoppingList.objects.filter(list_name=json_data['listname'])[0]
-        print(itemlist.item_key)
-        print(itemlist.user_key)
-        print(itemlist.list_key)
         itemlist.save()
     return response
 

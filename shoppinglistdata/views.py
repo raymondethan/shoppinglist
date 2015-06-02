4297a454de57037768ad1ec38b2bf516e03c7832
Ethan Raymond <ethan.raymond14@gmail.com>
2015-06-01 23:30:17 -0500

    completed

--- shoppinglistdata/views.py
+++ shoppinglistdata/views.py
@@ -85,17 +85,14 @@ def login(request):
 def updatecompleted(request):
     response = HttpResponse()
     if request.method == 'POST':
-        print("HI")
-        sys.stdout.flush()
         data = json.loads(request.body)
-        print(data)
-        sys.stdout.flush()
         user = User.objects.filter(username = data["username"])[0]
-        print(user.username)
-        sys.stdout.flush()
         for item in data["items"]:
             print(item)
-            i = ItemList.objects.filter(item_key__item_name=item, list_key__list_name=user.username+"'_list", user_key=user)[0]
+            try:
+                i = ItemList.objects.filter(item_key__item_name=item, list_key__list_name=user.username+"'_list", user_key=user)[0]
+            except:
+                print("failed")
             i.completed = True
             i.save()
     return response
